abe32e49c1c5a2b14e763b5cba6404897b4ba92b
Thais Rosa <tsrosa18@gmail.com>
2023-05-13 10:47:35 -0300

    Criação da Classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+  
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+  
+  public static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+  
+  public get name(): string {
+    return this.name;
+  }
+  
+  public get dexterity(): string {
+    return this.dexterity;
+  }
+  
+  public abstract get maxLifePoints(): number;
+}
+  
+export default Race;
